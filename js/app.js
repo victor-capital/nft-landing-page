a2efd954da1f5fdd53d93cce2c9ab7d5934f6fb9
victor-capital <akainruvictorifeanyi@gmail.com>
2022-07-11 21:35:22 +0800

    Updated Index file, app.js folder and css files

--- js/app.js
+++ js/app.js
@@ -1,133 +1,241 @@
-// METAMASK CONNECTION
-const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
-let editions = [];
-let dots = 1;
-
-window.addEventListener('DOMContentLoaded', () => {
-  const onboarding = new MetaMaskOnboarding();
-  const onboardButton = document.getElementById('connectWallet');
-  let accounts;
-
-  const updateButton = async () => {
-    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
-      onboardButton.innerText = 'Install MetaMask!';
-      onboardButton.onclick = () => {
-        onboardButton.innerText = 'Connecting...';
-        onboardButton.disabled = true;
-        onboarding.startOnboarding();
-      };
-    } else if (accounts && accounts.length > 0) {
-      onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
-      onboardButton.disabled = true;
-      onboarding.stopOnboarding();
-      checkOwner(accounts[0]);
-    } else {
-      onboardButton.innerText = 'Connect MetaMask!';
-      onboardButton.onclick = async () => {
-        await window.ethereum.request({
-          method: 'eth_requestAccounts',
-        })
-        .then(function(accounts) {
-          onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
-          onboardButton.disabled = true;
-          checkOwner(accounts[0]);
-        });
-      };
-    }
-  };
-
-  updateButton();
-  if (MetaMaskOnboarding.isMetaMaskInstalled()) {
-    window.ethereum.on('accountsChanged', (newAccounts) => {
-      accounts = newAccounts;
-      updateButton();
-    });
-  }
-});
-
-const checkOwner = async (account) => {
-  if(account) {
-    let isOwner = false;
-    let page = 1
-    
-    const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
-
-    isOwner = !isOwner ? data.isOwner : isOwner;
-    updateStatusText(isOwner, true)
-    
-    editions = [...data.editions]
-    let nextPage = data.next_page
-
-    while(nextPage) {
-      page = nextPage
-      const data = await fetchWithRetry(`/.netlify/functions/isowner/?wallet=${account}&page=${page}`);
-
-      isOwner = !isOwner ? data.isOwner : isOwner;
-      updateStatusText(isOwner, true)
+// vROWD
+console.clear()
+      console.log('lsakdfalskjdflnksd')
       
-      editions = [...editions, ...data.editions]
-      nextPage = data.next_page
-    }
-
-    updateStatusText(isOwner, false)
-  }
-}
-
-function updateStatusText(isOwner, checking) {
-  const statusText = document.querySelector('.owner-status');
-  if(checking) {
-    if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
-    } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
-    }
-  } else {
-    if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
-    } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
-    }
-  }
-  dots = dots === 3 ? 1 : dots + 1;
-}
-
-function renderDots(dots) {
-  let dotsString = '';
-  for (let i = 0; i < dots; i++) {
-    dotsString += '.';
-  }
-  return dotsString;
-}
-
-function timer(ms) {
-  return new Promise(res => setTimeout(res, ms));
-}
-
-async function fetchWithRetry(url)  {
-  await timer(TIMEOUT);
-  return new Promise((resolve, reject) => {
-    const fetch_retry = (_url) => {
-      return fetch(_url).then(async (res) => {
-        const status = res.status;
-
-        if(status === 200) {
-          return resolve(res.json());
-        }            
-        else {
-          console.error(`ERROR STATUS: ${status}`)
-          console.log('Retrying')
-          await timer(TIMEOUT)
-          fetch_retry(_url)
-        }            
-      })
-      .catch(async (error) => {  
-        console.error(`CATCH ERROR: ${error}`)  
-        console.log('Retrying')    
-        await timer(TIMEOUT)    
-        fetch_retry(_url)
-      }); 
-    }
-    return fetch_retry(url);
-  });
-}
+      const config = {
+        src: './././images/header/open-peeps-sheet.png',
+        rows: 15,
+        cols: 7
+      }
+      
+      // UTILS
+      
+      const randomRange = (min, max) => min + Math.random() * (max - min)
+      
+      const randomIndex = (array) => randomRange(0, array.length) | 0
+      
+      const removeFromArray = (array, i) => array.splice(i, 1)[0]
+      
+      const removeItemFromArray = (array, item) => removeFromArray(array, array.indexOf(item))
+      
+      const removeRandomFromArray = (array) => removeFromArray(array, randomIndex(array))
+      
+      const getRandomFromArray = (array) => (
+        array[randomIndex(array) | 0]
+      )
+      
+      // TWEEN FACTORIES
+      
+      const resetPeep = ({ stage, peep }) => {
+        const direction = Math.random() > 0.5 ? 1 : -1
+        // using an ease function to skew random to lower values to help hide that peeps have no legs
+        const offsetY = 100 - 250 * gsap.parseEase('power2.in')(Math.random())
+        const startY = stage.height - peep.height + offsetY
+        let startX
+        let endX
+        
+        if (direction === 1) {
+          startX = -peep.width
+          endX = stage.width
+          peep.scaleX = 1
+        } else {
+          startX = stage.width + peep.width
+          endX = 0
+          peep.scaleX = -1
+        }
+        
+        peep.x = startX
+        peep.y = startY
+        peep.anchorY = startY
+        
+        return {
+          startX,
+          startY,
+          endX
+        }
+      }
+      
+      const normalWalk = ({ peep, props }) => {
+        const {
+          startX,
+          startY,
+          endX
+        } = props
+      
+        const xDuration = 10
+        const yDuration = 0.25
+        
+        const tl = gsap.timeline()
+        tl.timeScale(randomRange(0.5, 1.5))
+        tl.to(peep, {
+          duration: xDuration,
+          x: endX,
+          ease: 'none'
+        }, 0)
+        tl.to(peep, {
+          duration: yDuration,
+          repeat: xDuration / yDuration,
+          yoyo: true,
+          y: startY - 10
+        }, 0)
+          
+        return tl
+      }
+      
+      const walks = [
+        normalWalk,
+      ]
+      
+      // CLASSES
+      
+      class Peep {
+        constructor({
+          image,
+          rect,
+        }) {
+          this.image = image
+          this.setRect(rect)
+          
+          this.x = 0
+          this.y = 0
+          this.anchorY = 0
+          this.scaleX = 1
+          this.walk = null
+        }
+        
+        setRect (rect) {
+          this.rect = rect
+          this.width = rect[2]
+          this.height = rect[3]
+          
+          this.drawArgs = [
+            this.image,
+            ...rect,
+            0, 0, this.width, this.height
+          ]  
+        }
+        
+        render (ctx) {
+          ctx.save()
+          ctx.translate(this.x, this.y)
+          ctx.scale(this.scaleX, 1)
+          ctx.drawImage(...this.drawArgs)
+          ctx.restore()
+        }
+      }
+      
+      // MAIN
+      
+      const img = document.createElement('img')
+      img.onload = init
+      img.src = config.src
+      
+      const canvas = document.querySelector('#canvas')
+      const ctx = canvas.getContext('2d')
+      
+      const stage = {
+        width: 0,
+        height: 0,
+      }
+      
+      const allPeeps = []
+      const availablePeeps = []
+      const crowd = []
+      
+      function init () {  
+        createPeeps()
+        
+        // resize also (re)populates the stage
+        resize()
+      
+        gsap.ticker.add(render)
+        window.addEventListener('resize', resize)
+      }
+      
+      function createPeeps () {
+        const {
+          rows,
+          cols
+        } = config
+        const {
+          naturalWidth: width,
+          naturalHeight: height
+        } = img
+        const total = rows * cols
+        const rectWidth = width / rows
+        const rectHeight = height / cols
+        
+        for (let i = 0; i < total; i++) {
+          allPeeps.push(new Peep({
+            image: img,
+            rect: [
+              (i % rows) * rectWidth,
+              (i / rows | 0) * rectHeight,
+              rectWidth,
+              rectHeight,
+            ]
+          }))
+        }  
+      }
+      
+      function resize () {
+        stage.width = canvas.clientWidth
+        stage.height = canvas.clientHeight
+        canvas.width = stage.width * devicePixelRatio
+        canvas.height = stage.height * devicePixelRatio
+        
+        crowd.forEach((peep) => {
+          peep.walk.kill()
+        })
+        
+        crowd.length = 0
+        availablePeeps.length = 0
+        availablePeeps.push(...allPeeps)
+        
+        initCrowd()
+      }
+      
+      function initCrowd () {
+        while (availablePeeps.length) {
+          // setting random tween progress spreads the peeps out
+          addPeepToCrowd().walk.progress(Math.random())
+        }
+      }
+      
+      function addPeepToCrowd () {
+        const peep = removeRandomFromArray(availablePeeps)
+        const walk = getRandomFromArray(walks)({
+          peep,
+          props: resetPeep({
+            peep,
+            stage,
+          })
+        }).eventCallback('onComplete', () => {
+          removePeepFromCrowd(peep)
+          addPeepToCrowd()
+        })
+        
+        peep.walk = walk
+        
+        crowd.push(peep)
+        crowd.sort((a, b) => a.anchorY - b.anchorY)
+        
+        return peep
+      }
+      
+      function removePeepFromCrowd (peep) {
+        removeItemFromArray(crowd, peep)
+        availablePeeps.push(peep)
+      }
+      
+      function render () {
+        canvas.width = canvas.width
+        ctx.save()
+        ctx.scale(devicePixelRatio, devicePixelRatio)
+        
+        crowd.forEach((peep) => {
+          peep.render(ctx)
+        })
+        
+        ctx.restore()
+      }
